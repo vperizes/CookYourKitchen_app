42cc60055481fbcb41eb1faba3b55c175fb2765f
vperizes <vperizes@gmail.com>
2023-10-09 19:47:19 -0500

    Add service checking logic to see if render service has been back up. If not, preloader will be shown

--- public/loadwebpage.js
+++
@@ -1,5 +0,0 @@
-
-$(window).on("load", () => {
-    $("#loader").fadeOut(1000);
-    $("#content").fadeIn(1000);
-});
---
+++ public/preloadwebpage.js
@@ -0,0 +1,28 @@
+
+// Function to check if the service is up using async/await
+async function isServiceUp() {
+    try {
+      const response = await fetch("/"); 
+      console.log(response);
+
+      //check if response was successful (status in 200-299 range)
+      if (response.ok) {
+        return true; // Service is up
+      } else {
+        return false; // Service is down
+      }
+    } catch (error) {
+      console.error('Error checking service status:', error);
+      return false; // Service is down or unreachable
+    }
+  }
+
+$(document).ready( () => {
+    if(isServiceUp){
+        $("#loader").fadeOut(1000);
+        $("#content").fadeIn(1000);
+    } else {
+        $("#loader").show();
+    }
+    
+});
