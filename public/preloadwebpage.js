cc09cd47f55a2bec3355c7a726e87180f807604f
vperizes <vperizes@gmail.com>
2023-10-12 15:12:27 -0500

    update preload script

--- public/preloadwebpage.js
+++ public/preloadwebpage.js
@@ -2,21 +2,21 @@
 // Function to check if the service is up using async/await
 async function isServiceUp() {
     try {
-      const response = await fetch("https://cookyourkitchen.onrender.com"); 
+      const response = await fetch("/"); 
       console.log(response);
 
     } catch (error) {
       console.error('Error checking service status:', error);
-      return false; // Service is down or unreachable
     }
   }
 
-$(document).ready( () => {
-    if(isServiceUp()){
+$(document).ready(() => {
+  var isUp = isServiceUp();
+  console.log(isUp);
+    if(isUp){
         $("#loader").fadeOut(1000);
         $("#content").fadeIn(1000);
     } else {
         $("#loader").show();
     }
-    
 });
