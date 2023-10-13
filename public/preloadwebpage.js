daacbfe0d08ffccf865bbef95ddf5800620f9066
vperizes <vperizes@gmail.com>
2023-10-12 21:38:09 -0500

    Update preloader animation. Implement periodic check for service status

--- public/preloadwebpage.js
+++ public/preloadwebpage.js
@@ -1,20 +1,32 @@
 
 $(document).ready(async () => {
   var isUp = await isServiceUp();
-  console.log(`Response: ${isUp}`);
-  if (isUp) {
-    $("#loader").fadeOut(1000);
-    $("#content").fadeIn(1000);
-  } else {
-    $("#loader").show();
-  }
+  console.log(`Service: ${isUp}`);
+
+  //periodic check for service status. checks every 2 seconds
+  setInterval( () => {
+    if(isUp){
+      $("#loader").fadeOut(1000);
+      $("#content").fadeIn(1000);
+    } else {
+      $("#loader").show();
+    }
+  }, 2000); 
+
+
+  // if (isUp) {
+  //   $("#loader").fadeOut(1000);
+  //   $("#content").fadeIn(1000);
+  // } else {
+  //   $("#loader").show();
+  // }
 });
 
 // Function to check if the service is up using async/await
 async function isServiceUp() {
   try {
-    const response = await fetch("https://cookyourkitchen.onrender.com");
-    console.log(`Response: ${response}`);
+    const response = await fetch("/");
+    console.log(`Response: ${response.ok}`);
     if (response.ok) {
       return true; // Service is up
     } else {
