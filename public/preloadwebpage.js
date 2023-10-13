6fdb10c75919767a311325e1a1525afccaa85906
vperizes <vperizes@gmail.com>
2023-10-12 19:26:31 -0500

    update preload script to explicitly return a ture/false value for isServiceUp()

--- public/preloadwebpage.js
+++ public/preloadwebpage.js
@@ -1,22 +1,27 @@
 
+$(document).ready(async () => {
+  var isUp = await isServiceUp();
+  console.log(isUp);
+  if (isUp) {
+    $("#loader").fadeOut(1000);
+    $("#content").fadeIn(1000);
+  } else {
+    $("#loader").show();
+  }
+});
+
 // Function to check if the service is up using async/await
 async function isServiceUp() {
-    try {
-      const response = await fetch("/"); 
-      console.log(response);
-
-    } catch (error) {
-      console.error('Error checking service status:', error);
-    }
-  }
-
-$(document).ready(() => {
-  var isUp = isServiceUp();
-  console.log(isUp);
-    if(isUp){
-        $("#loader").fadeOut(1000);
-        $("#content").fadeIn(1000);
+  try {
+    const response = await fetch("/");
+    if (response.ok) {
+      return true; // Service is up
     } else {
-        $("#loader").show();
+      return false; // Service is down
     }
-});
+
+  } catch (error) {
+    console.error('Error checking service status:', error);
+    return false; // Service is down (due to error)
+  }
+}
