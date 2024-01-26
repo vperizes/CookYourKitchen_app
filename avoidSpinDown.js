7080ab5b0b154059672606e084c232aadbb574cd
vperizes <vperizes@gmail.com>
2024-01-26 01:06:50 -0600

    add script to avoid server spin down

---
+++ avoidSpinDown.js
@@ -0,0 +1,30 @@
+import https from "https";
+
+//code to make request to render.com (keep server from spinning down)
+export const makeRequest = () => {
+  // Replace 'example.com' with the actual domain of the website you want to ping
+  const url = "https://careercompass-0z1a.onrender.com/";
+
+  try {
+    https
+      .get(url, (response) => {
+        let data = "";
+
+        // event listener for data event. Appends each chunk of recieved data to data variable
+        response.on("data", (chunk) => {
+          data += chunk;
+        });
+
+        //Event listener for 'end' event, indicates entire response has been received.
+        response.on("end", () => {
+          console.log("Request successful:", data);
+        });
+      })
+      //error event listener. msg logged if error occurs during request
+      .on("error", (error) => {
+        console.error("Error making request:", error.message);
+      });
+  } catch (error) {
+    console.error("Caught an exception:", error.message);
+  }
+};
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
-import 'dotenv/config';
+import "dotenv/config";
+import { makeRequest } from "./avoidSpinDown.js";
 
 const app = express();
 const port = 3000;
@@ -15,44 +16,45 @@ app.use(express.static("public"));
 let nextPageUrl = null;
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
-})
+  res.render("index.ejs");
+});
 
 app.post("/search", async (req, res) => {
-    const inputText = req.body.ingredients;
-    try{
-        const requestUrl = nextPageUrl || API_URL;
+  const inputText = req.body.ingredients;
+  try {
+    const requestUrl = nextPageUrl || API_URL;
 
-        const response = await axios.get(requestUrl, {
-            params: {
-                q: inputText,
-                app_id: myId,
-                app_key: myAPIKey
-            }
-        });
-        const result = response.data;
+    const response = await axios.get(requestUrl, {
+      params: {
+        q: inputText,
+        app_id: myId,
+        app_key: myAPIKey,
+      },
+    });
+    const result = response.data;
 
-        //store next page url for pagination
-        if(result._links.next.href){
-            nextPageUrl = result._links.next.href;
-        } else{
-            nextPageUrl = null; //no more pages/recipes given the user search
-        }
-
-        res.render("index.ejs", {
-            recipes: result,
-            userInput: inputText,
-            nextPageUrl: nextPageUrl
-        });
-    }catch(error){
-        res.render("index.ejs", {
-            content: "Looks like there's an issue: " + error.message
-        });
-
+    //store next page url for pagination
+    if (result._links.next.href) {
+      nextPageUrl = result._links.next.href;
+    } else {
+      nextPageUrl = null; //no more pages/recipes given the user search
     }
-})
 
+    res.render("index.ejs", {
+      recipes: result,
+      userInput: inputText,
+      nextPageUrl: nextPageUrl,
+    });
+  } catch (error) {
+    res.render("index.ejs", {
+      content: "Looks like there's an issue: " + error.message,
+    });
+  }
+});
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}.`);
-})
+  console.log(`Server listening on port ${port}.`);
+});
+
+// Call request function every 14.5 minutes to avid server spin down
+setTimeout(makeRequest, 1000 * 62 * 14);
