8f01555ac8bb7f8ed03541b6024b60e045558794
vperizes <vperizes@gmail.com>
2024-01-26 02:54:23 -0600

    add anchor tag to recipe card. update css. clean up peloader

--- public/preloadwebpage.js
+++ public/preloadwebpage.js
@@ -1,36 +1,18 @@
-
 // Function to check if the service is up using async/await
 async function isServiceUp() {
   try {
     const response = await fetch("/");
     console.log(`Response: ${response.ok}`);
     return true; //service is up
-    // if (response.ok) {
-    //   return true; // Service is up
-    // } else {
-    //   return false; // Service is down
-    // }
-
   } catch (error) {
-    console.error('Error checking service status:', error);
+    console.error("Error checking service status:", error);
     return false; // Service is down (due to error)
   }
 }
 
-
 $(document).ready(async () => {
   var isUp = await isServiceUp();
   console.log(`Service: ${isUp}`);
-
-  // //periodic check for service status. checks every 2 seconds
-  // setInterval( () => {
-  //   if(isUp){
-  //     $("#loader").fadeOut(1000);
-  //     $("#content").fadeIn(1000);
-  //   } else {
-  //     $("#loader").show();
-  //   }
-  // }, 1000); 
 
   if (isUp) {
     $("#loader").fadeOut(1000);
@@ -39,4 +21,3 @@ $(document).ready(async () => {
     $("#loader").show();
   }
 });
-
